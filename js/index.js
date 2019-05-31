13d224a8959b4ee368579505d0d906b18e9c8f09
Kyza <40003440+KyzaGitHub@users.noreply.github.com>
2019-05-31 00:18:41 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 ;(function () {
     var src = 'https://cdnjs.cloudflare.com/ajax/libs/eruda/1.5.5/eruda.min.js';
-    alert("nut");
     if (!getQueryVariable("eruda") == "true" && localStorage.getItem('active-eruda') != 'true') return;
-    alert("nut");
     document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
     document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
 })();
