a82a40b6a10e89475ed5ae224e83d51052a01175
Kyza <40003440+KyzaGitHub@users.noreply.github.com>
2019-05-30 23:45:01 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 ;(function () {
-    var src = 'node_modules/eruda/eruda.min.js';
+    var src = '//cdn.jsdelivr.net/npm/eruda';
     if (!/eruda=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
     document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
     document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
