cad350f18b7093a14488ed312a63d98266dac2f6
KyzaGitHub <tylerc2077@gmail.com>
2019-06-06 13:04:12 -0400

    Update

--- js/index.js
+++ js/index.js
@@ -103,31 +103,36 @@ function openDownloadPopup(downloadName, downloadLocation) {
         var type = downloadLocation.replace("v1", "plugin").replace("v2", "plugin");
         var pluginThemeURL = "https://kyza.gq/Khub/" + downloadPath + "/" + downloadName + "/" + downloadName + "." + type + "." + type.replace("plugin", "js").replace("theme", "css");
         // Get the raw plugin/theme data and save it to a variable.
-        $.get(pluginThemeURL, function(response) {
-          var pluginThemeRaw = response;
+        $.ajax({
+          type: 'GET',
+          url: pluginThemeURL,
+          dataType: 'text',
+          success: function(response) {
+            var pluginThemeRaw = response;
 
-          if (type == "plugin") pluginThemeRaw = js_beautify(pluginThemeRaw, {
-            indent_size: 2,
-            space_in_empty_paren: false
-          });
-          if (type == "theme") pluginThemeRaw = css_beautify(pluginThemeRaw, {
-            indent_size: 2,
-            space_in_empty_paren: false
-          });
-          console.log(pluginThemeRaw);
+            if (type == "plugin") pluginThemeRaw = js_beautify(pluginThemeRaw, {
+              indent_size: 2,
+              space_in_empty_paren: false
+            });
+            if (type == "theme") pluginThemeRaw = css_beautify(pluginThemeRaw, {
+              indent_size: 2,
+              space_in_empty_paren: false
+            });
+            console.log(pluginThemeRaw);
 
-          var element = document.createElement('a');
-          element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(pluginThemeRaw));
+            var element = document.createElement('a');
+            element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(pluginThemeRaw));
 
-          if (getQueryVariable("theme")) {
-            element.setAttribute('download', downloadName + "." + type + ".css");
-          } else {
-            element.setAttribute('download', downloadName + "." + type + ".js");
+            if (getQueryVariable("theme")) {
+              element.setAttribute('download', downloadName + "." + type + ".css");
+            } else {
+              element.setAttribute('download', downloadName + "." + type + ".js");
+            }
+            element.style.display = 'none';
+            document.body.appendChild(element);
+            element.click();
+            document.body.removeChild(element);
           }
-          element.style.display = 'none';
-          document.body.appendChild(element);
-          element.click();
-          document.body.removeChild(element);
         });
       });
       try {
