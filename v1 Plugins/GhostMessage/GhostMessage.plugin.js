6ca6beb626a088379b634a56403ab9f9110ca8c1
Kyza <TylerC2077@gmail.com>
2019-09-10 17:20:49 -0400

    Fixed visuals for GhostMessage

--- v1 Plugins/GhostMessage/GhostMessage.plugin.js
+++ v1 Plugins/GhostMessage/GhostMessage.plugin.js
@@ -27,15 +27,11 @@ GhostMessage.prototype.start = function() {
 
   addButton();
 
-  ZLibrary.Patcher.before(ZLibrary.DiscordModules.MessageActions, "sendMessage", (thisObject /*, methodArguments, returnValue*/ ) => {
-    console.log(thisObject);
-  });
-
-	patch();
+  patch();
 };
 
 function patch() {
-	try {
+  try {
     unpatch();
   } catch (e) {}
 
@@ -55,7 +51,7 @@ function patch() {
       if (enabled) {
         console.log(e);
       }
-			return e;
+      return e;
     }
   });
 }
@@ -72,13 +68,13 @@ GhostMessage.prototype.load = function() {
 
 GhostMessage.prototype.unload = function() {
   removeButton();
-	unpatch();
+  unpatch();
 };
 
 GhostMessage.prototype.stop = function() {
   clearInterval(updateInterval);
   removeButton();
-	unpatch();
+  unpatch();
 };
 
 GhostMessage.prototype.onMessage = function() {
@@ -101,10 +97,10 @@ function addButton() {
         var daButtons = document.getElementsByClassName("buttons-205you")[0];
         var ghostButton = document.createElement("button");
         ghostButton.setAttribute("type", "button");
-        ghostButton.setAttribute("class", "buttonWrapper-1ZmCpA da-buttonWrapper button-38aScr da-button lookBlank-3eh9lL da-lookBlank colorBrand-3pXr91 da-colorBrand grow-q77ONN da-grow normal ghost-button-wrapper");
+        ghostButton.setAttribute("class", "buttonWrapper-1ZmCpA button-38aScr lookBlank-3eh9lL colorBrand-3pXr91 grow-q77ONN ghost-button-wrapper");
 
         var ghostButtonInner = document.createElement("div");
-        ghostButtonInner.setAttribute("class", "contents-18-Yxp da-contents button-3AYNKb da-button button-2vd_v_ da-button ghost-button-inner");
+        ghostButtonInner.setAttribute("class", "contents-18-Yxp button-3AYNKb button-2vd_v_ ghost-button-inner");
 
         //<img src="https://image.flaticon.com/icons/svg/24/24207.svg" width="224" height="224" alt="Embed free icon" title="Embed free icon">
         //<svg version="1.1" xmlns="http://www.w3.org/2000/svg" class="icon-3D60ES da-icon" viewBox="0 0 22 22" fill="currentColor"><path d="M 19.794, 3.299 H 9.765 L 8.797, 0 h -6.598 C 0.99, 0, 0, 0.99, 0, 2.199 V 16.495 c 0, 1.21, 0.99, 2.199, 2.199, 2.199 H 9.897 l 1.1, 3.299 H 19.794 c 1.21, 0, 2.199 -0.99, 2.199 -2.199 V 5.498 C 21.993, 4.289, 21.003, 3.299, 19.794, 3.299 z M 5.68, 13.839 c -2.48, 0 -4.492 -2.018 -4.492 -4.492 s 2.018 -4.492, 4.492 -4.492 c 1.144, 0, 2.183, 0.407, 3.008, 1.171 l 0.071, 0.071 l -1.342, 1.298 l -0.066 -0.06 c -0.313 -0.297 -0.858 -0.643 -1.671 -0.643 c -1.441, 0 -2.612, 1.193 -2.612, 2.661 c 0, 1.468, 1.171, 2.661, 2.612, 2.661 c 1.507, 0, 2.161 -0.962, 2.337 -1.606 h -2.43 v -1.704 h 4.344 l 0.016, 0.077 c 0.044, 0.231, 0.06, 0.434, 0.06, 0.665 C 10.001, 12.036, 8.225, 13.839, 5.68, 13.839 z M 11.739, 9.979 h 4.393 c 0, 0 -0.374, 1.446 -1.715, 3.008 c -0.588 -0.676 -0.995 -1.336 -1.254 -1.864 h -1.089 L 11.739, 9.979 z M 13.625, 13.839 l -0.588, 0.583 l -0.72 -2.452 C 12.685, 12.63, 13.13, 13.262, 13.625, 13.839 z M 20.893, 19.794 c 0, 0.605 -0.495, 1.1 -1.1, 1.1 H 12.096 l 2.199 -2.199 l -0.896 -3.041 l 1.012 -1.012 l 2.953, 2.953 l 0.803 -0.803 l -2.975 -2.953 c 0.99 -1.138, 1.759 -2.474, 2.106 -3.854 h 1.397 V 8.841 H 14.697 v -1.144 h -1.144 v 1.144 H 11.398 l -1.309 -4.443 H 19.794 c 0.605, 0, 1.1, 0.495, 1.1, 1.1 V 19.794 z"></path></svg>
@@ -112,8 +108,8 @@ function addButton() {
         var ghostButtonIcon = document.createElement("img");
         //version="1.1" xmlns="http://www.w3.org/2000/svg" class="icon-3D60ES da-icon" viewBox="0 0 22 22" fill="currentColor"
         ghostButtonIcon.setAttribute("src", "https://image.flaticon.com/icons/svg/121/121202.svg");
-        ghostButtonIcon.setAttribute("class", "icon-3D60ES da-icon");
-        ghostButtonIcon.setAttribute("style", "filter: invert(100%) !important;");
+        ghostButtonIcon.setAttribute("class", "icon-3D60ES");
+        ghostButtonIcon.setAttribute("style", "filter: invert(75%) !important;");
         ghostButtonIcon.setAttribute("width", "22");
         ghostButtonIcon.setAttribute("height", "22");
 
@@ -157,10 +153,10 @@ function setEnabled(set) {
   var ghostInner = document.getElementsByClassName("ghost-button-inner")[0];
   if (ghostInner && ghostInner.children[0] && enabled) {
     ghostInner.setAttribute("style", "opacity: 1; transform: none;");
-    ghostInner.children[0].setAttribute("style", "filter: invert(100%) !important; opacity: 1; transform: none;");
+    ghostInner.children[0].setAttribute("style", "filter: invert(100%) !important; transform: scale(1.3);");
   } else if (ghostInner && ghostInner.children[0] && !enabled) {
     ghostInner.setAttribute("style", "");
-    ghostInner.children[0].setAttribute("style", "filter: invert(100%) !important;");
+    ghostInner.children[0].setAttribute("style", "filter: invert(75%) !important;");
   }
 }
 
@@ -233,7 +229,7 @@ GhostMessage.prototype.getDescription = function() {
 };
 
 GhostMessage.prototype.getVersion = function() {
-  return "1.0.1";
+  return "1.0.2";
 };
 
 GhostMessage.prototype.getAuthor = function() {
