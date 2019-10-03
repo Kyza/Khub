d67ce9ea2f4795f7e10be7a0678ab892e3ea39f5
Kyza <TylerC2077@gmail.com>
2019-10-03 11:01:27 -0400

    Basic KSS editor released! Try Alt+K.

--- Libraries/KSS/1KSSLibrary.plugin.js
+++ Libraries/KSS/1KSSLibrary.plugin.js
@@ -32,6 +32,11 @@ String.prototype.replaceAll = function(find, replace) {
     replace
   );
 };
+String.prototype.splice = function(start, delCount, newSubStr) {
+  return (
+    this.slice(0, start) + newSubStr + this.slice(start + Math.abs(delCount))
+  );
+};
 /* STOP: Utility Functions */
 
 /* START: Library */
@@ -58,25 +63,27 @@ function KSSLibrary(plugin) {
 
     // Lighty
     const parsed = kss.match(/\|.*?\|/g);
-    for (const i of parsed) {
-      const search = i.substr(1, i.length - 2).split(" ");
-      const res = ZLibrary.WebpackModules.getByProps(...search);
-      if (!res || !res[search[search.length - 1]]) {
-        ZLibrary.Logger.warn(
-          plugin.getName(),
-          `Could not find selector for "${search.join(", ")}"!`
-        );
-        ZLibrary.Toasts.error(
-          `${plugin.getName()}: Could not find selector for "${search.join(
-            ", "
-          )}"!`
-        );
-      } else {
-        try {
-          const result = "." + res[search[search.length - 1]].split(" ")[0];
-          const escaped = i.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-          kss = kss.replace(new RegExp(escaped, "g"), result);
-        } catch (e) {}
+    if (parsed) {
+      for (const i of parsed) {
+        const search = i.substr(1, i.length - 2).split(" ");
+        const res = ZLibrary.WebpackModules.getByProps(...search);
+        if (!res || !res[search[search.length - 1]]) {
+          ZLibrary.Logger.warn(
+            plugin.getName(),
+            `Could not find selector for "${search.join(", ")}"!`
+          );
+          ZLibrary.Toasts.error(
+            `${plugin.getName()}: Could not find selector for "${search.join(
+              ", "
+            )}"!`
+          );
+        } else {
+          try {
+            const result = "." + res[search[search.length - 1]].split(" ")[0];
+            const escaped = i.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+            kss = kss.replace(new RegExp(escaped, "g"), result);
+          } catch (e) {}
+        }
       }
     }
 
@@ -152,7 +159,7 @@ function KSSLibrary(plugin) {
             }
           );
         } else {
-          console.log("Can't update.");
+          //   console.log("Can't update.");
         }
       }, 30e3)
     };
@@ -250,17 +257,21 @@ function KSSLibrary(plugin) {
 
   // Lighty
   this.findSelectors = (sel) => {
-    let ret = '';
-    const selector = sel.split('-')[0];
-    const matches = mod => mod && typeof mod[selector] === 'string' && mod[selector].split(' ')[0] === sel;
-    if (matches(ZLibrary.WebpackModules.getByProps(selector))) return console.info(selector), selector;
-    ZLibrary.WebpackModules.find(m => {
+    let ret = "";
+    const selector = sel.split("-")[0];
+    const matches = (mod) =>
+      mod &&
+      typeof mod[selector] === "string" &&
+      mod[selector].split(" ")[0] === sel;
+    if (matches(ZLibrary.WebpackModules.getByProps(selector)))
+      return console.info(selector), selector;
+    ZLibrary.WebpackModules.find((m) => {
       if (ret || !matches(m)) return false;
       const keys = Object.keys(m);
       let args = [];
       const baseIdx = keys.indexOf(selector);
       goNegative = baseIdx + 1 >= keys.length;
-      const getNextIdx = idx => {
+      const getNextIdx = (idx) => {
         let rr = idx - 1;
         if (rr === baseIdx) rr--;
         return rr;
@@ -277,10 +288,10 @@ function KSSLibrary(plugin) {
           const result = [];
           for (let i = 0; i < args.length; i++) result.push(keys[args[i]]);
           result.push(selector);
-          ret = result.join(' ');
+          ret = result.join(" ");
           break;
         }
-        const incrementNext = cur => {
+        const incrementNext = (cur) => {
           let newVar = getNextIdx(args[cur]);
           if (newVar < 0 || newVar >= keys.length) {
             if (cur === args.length - 1) {
@@ -291,7 +302,7 @@ function KSSLibrary(plugin) {
               incrementNext(cur + 1);
               newVar = getNextIdx(args[cur + 1]);
             }
-            if (newVar < 0 || newVar >= keys.length) throw 'Fuck me';
+            if (newVar < 0 || newVar >= keys.length) throw "Fuck me";
           }
           args[cur] = newVar;
         };
@@ -335,6 +346,249 @@ function KSSLibrary(plugin) {
 /* STOP: Library */
 
 window.KSSLibrary = KSSLibrary;
+
+/* START: Handle Overlay */
+
+var KSS = new window.KSSLibrary({
+  getVersion: () => {
+    return "0.0.0";
+  },
+  getName: () => {
+    return "PluginName";
+  }
+});
+
+function updateCSS() {
+  KSS.setModule("userKSS", "");
+  KSS.setModule(
+    "overlay-styles",
+    `
+@import url("https://cdn.jsdelivr.net/gh/tonsky/FiraCode@master/distr/fira_code.css");
+
+#KSSOverlay {
+font-family: "Fira Code", monospace;
+
+top: ${KSS.currentPlatform() == "win" ? "22" : "0"}px;
+left: 1px;
+width: calc(100% - 1px);
+height: calc(100% - ${KSS.currentPlatform() == "win" ? "22" : "0"}px);
+
+background-color: black;
+
+opacity: 0.6;
+
+position: absolute;
+z-index: 999;
+
+transition-duration: 0.5s;
+pointer-events: auto;
+}
+
+.hideKSSOverlay {
+opacity: 0 !important;
+pointer-events: none !important;
+}
+
+#KSSOverlayTextarea {
+position: absolute;
+top: 44px;
+left: 0px;
+width: 100%;
+height: calc(100% - 45px);
+
+background-color: rgba(255, 255, 255, 0.0);
+border: none;
+border-top: 1px white dotted;
+padding: 0px;
+color: white;
+
+font-size: 24px;
+font-family: "Fira Code";
+overflow-x: auto;
+white-space: nowrap;
+
+resize: none;
+
+tab-size: 2;
+}
+    `
+  );
+}
+
+function removeCSS() {
+  try {
+    KSS.disposeModule("userKSS");
+    KSS.disposeModule("overlay-styles");
+  } catch (e) {}
+}
+
+function loadDataString(key) {
+  var dataObject = ZLibrary.PluginUtilities.loadData("KSSLibrary", key, "");
+  console.log(key, dataObject);
+  var dataString = "";
+  for (let i = 0; i < Object.keys(dataObject).length; i++) {
+    dataString += dataObject[i];
+  }
+  return dataString;
+}
+
+function saveData(key, data) {
+  ZLibrary.PluginUtilities.saveData("KSSLibrary", key, data);
+}
+
+function addOverlay() {
+  if (document.querySelector("#KSSOverlay")) return;
+  var overlay = document.createElement("div");
+  overlay.id = "KSSOverlay";
+  overlay.className = "hideKSSOverlay";
+
+  var textarea = document.createElement("textarea");
+  textarea.id = "KSSOverlayTextarea";
+
+  textarea.value = loadDataString("editorKSS");
+  textarea.selectionStart = loadDataString("editorSelectionStart");
+  textarea.selectionEnd = loadDataString("editorSelectionEnd");
+
+  // Format and convert KSS.
+  textarea.oninput = (e) => {
+    if (e.data != null) {
+      if (textarea.value[textarea.selectionStart - 1] == "{") {
+        textarea.value = textarea.value.splice(
+          textarea.selectionStart,
+          0,
+          `\n\t\n}`
+        );
+        textarea.selectionStart = textarea.selectionStart - 2;
+        textarea.selectionEnd = textarea.selectionEnd - 2;
+      }
+    }
+
+    try {
+      let content = textarea.value;
+      const matched = content.match(
+        /(?<=\(?)(\.\D[^\.{;):,}[ ]*-*)(?=[ \.\n:),[ ])/g
+      );
+      for (const m of matched) {
+        const findPlz = m.substr(1);
+        const matches = KSS.findSelectors(findPlz);
+        if (!matches) {
+          console.warn(findPlz, "not found!");
+          continue;
+        }
+        const escaped = m.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+        textarea.value = content.replace(
+          new RegExp(escaped, "g"),
+          `|${matches}|`
+        );
+      }
+    } catch (e) {}
+
+    saveData("editorKSS", textarea.value);
+
+    setTimeout(() => {
+      KSS.setModule("userKSS", textarea.value);
+    }, 1e2);
+  };
+
+  textarea.onkeyup = saveSelection;
+  textarea.onmouseup = saveSelection;
+
+  // Handle tabs.
+  textarea.onkeydown = (e) => {
+    if (e.keyCode === 9) {
+      e.preventDefault();
+
+      var oldStart = textarea.selectionStart;
+      var oldEnd = textarea.selectionEnd;
+
+      textarea.value = textarea.value.splice(textarea.selectionStart, 0, `\t`);
+      saveData("editorKSS", textarea.value);
+
+      textarea.selectionStart = oldStart + 1;
+      textarea.selectionEnd = oldEnd + 1;
+
+      saveSelection();
+    }
+  };
+
+  overlay.appendChild(textarea);
+
+  overlay.onmouseout = () => {
+    overlay.setAttribute("style", "pointer-events: none;");
+    saveSelection();
+  };
+
+  document.onmouseover = () => {
+    overlay.setAttribute("style", "");
+  };
+
+  document.body.appendChild(overlay);
+
+  if (loadDataString("KSSOverlayOpen") == "true") {
+    toggleOverlay();
+  }
+}
+
+function saveSelection() {
+  saveData(
+    "editorSelectionStart",
+    document.querySelector("#KSSOverlayTextarea").selectionStart + ""
+  );
+  saveData(
+    "editorSelectionEnd",
+    document.querySelector("#KSSOverlayTextarea").selectionEnd + ""
+  );
+
+  document.querySelector("#KSSOverlayTextarea").selectionStart = loadDataString(
+    "editorSelectionStart"
+  );
+  document.querySelector("#KSSOverlayTextarea").selectionEnd = loadDataString(
+    "editorSelectionEnd"
+  );
+}
+
+function removeOverlay() {
+  try {
+    document.querySelector("#KSSOverlay").remove();
+  } catch (e) {}
+}
+
+function toggleOverlay() {
+  if (
+    document.querySelector("#KSSOverlay").className.indexOf("hideKSSOverlay") <
+    0
+  ) {
+    KSSOverlayEnabled = true;
+    document.querySelector("#KSSOverlay").className += " hideKSSOverlay";
+    document.querySelector("#KSSOverlay").className = document
+      .querySelector("#KSSOverlay")
+      .className.trim();
+    document.querySelector("#KSSOverlayTextarea").blur();
+    saveData("KSSOverlayOpen", "false");
+  } else {
+    document.querySelector("#KSSOverlay").className = document
+      .querySelector("#KSSOverlay")
+      .className.replaceAll("hideKSSOverlay", "")
+      .trim();
+    document.querySelector("#KSSOverlayTextarea").focus();
+    saveData("KSSOverlayOpen", "true");
+  }
+}
+
+document.onkeydown = () => {
+  var evtobj = window.event ? event : e;
+  if (evtobj.keyCode == 75 && evtobj.altKey) {
+    toggleOverlay();
+  }
+};
+
+removeOverlay();
+removeCSS();
+
+addOverlay();
+updateCSS();
+
+/* STOP: Handle Overlay */
 
 /* START: Handle KSS Theme Loading */
 /*
@@ -424,29 +678,31 @@ var KSSLibrary = (() => {
           github_username: "KyzaGitHub"
         }
       ],
-      version: "0.1.3",
+      version: "0.1.4",
       description: "Easy CSS for BetterDiscord.",
       github: "https://github.com/KyzaGitHub/Khub/tree/master/Libraries/KSS",
       github_raw:
         "https://raw.githubusercontent.com/KyzaGitHub/Khub/master/Libraries/KSS/1KSSLibrary.plugin.js"
     },
     changelog: [
-      // {
-      //   title: "New Stuff",
-      //   items: ["Added findSelectorsFast() and findSelectorsAccurate()."]
-      // }
-      // ,
-    //   {
-    //     title: "Bugs Squashed",
-    //     type: "fixed",
-    //     items: ["Fixed findSelectorsAccurate()."]
-    //   }
-      // ,
       {
-        "title": "Improvements",
-        "type": "improved",
-        "items": ["Changed findSelectorsAccurate() to findSelectors(), and fixed an error that occurs when trying to find some selectors."]
+        title: "New Stuff",
+        items: ["Added a simple KSS editor. Try Alt+K."]
       }
+      // ,
+      //   {
+      //     title: "Bugs Squashed",
+      //     type: "fixed",
+      //     items: ["Fixed findSelectorsAccurate()."]
+      //   }
+      // ,
+      //   {
+      //     title: "Improvements",
+      //     type: "improved",
+      //     items: [
+      //       "Changed findSelectorsAccurate() to findSelectors(), and fixed an error that occurs when trying to find some selectors."
+      //     ]
+      //   }
       // ,
       // {
       //   "title": "On-going",
